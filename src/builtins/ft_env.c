e2f7d7eafde5d7b8b07481b31e4a2aeb54997e39
NicoIasB <nicolas.berduck@gmail.com>
2024-03-19 17:48:17 +0100

    Add builtins

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nberduck <nberduck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 15:22:49 by tchartie          #+#    #+#             */
-/*   Updated: 2024/03/19 16:58:44 by nberduck         ###   ########.fr       */
+/*   Created: 2024/03/19 15:30:20 by nberduck          #+#    #+#             */
+/*   Updated: 2024/03/19 16:18:38 by nberduck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/libft.h"
+#include "../include/minishell.h"
 
-/*
-** Takes as a parameter a node and frees the memory of
-** the node’s content using the function ’del’ given
-** as a parameter and free the node. The memory of
-** ’next’ must not be freed
-*/
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+int	ft_env(char **envp)
 {
-	
-	if (!lst || !del)
-		return ;
-	del(lst->content);
-	free(lst);
+	unsigned int	i;
+
+	i = 0;
+	if (!envp)
+		return (1);
+	while (envp[i])
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
+	return (0);
 }
